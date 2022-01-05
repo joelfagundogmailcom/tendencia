c154db571024f728bf16374c36c4ffae192a03e4
JOEL <joelfacundo@MacBook-Pro-de-JOEL.local>
2022-01-05 02:26:08 -0600

    API log

--- routes/main.routes.js
+++ routes/main.routes.js
@@ -4,11 +4,18 @@ const router = require('express').Router();
 const prefix = '';
 
 const controller = require('../controllers/main.controller');
+const logs = require('../controllers/logs');
 
 router.get(`${prefix}/`, controller.all);
 router.post(`${prefix}/`, controller.create);
 router.get(`${prefix}/:id`, controller.info);
 router.put(`${prefix}/:id`, controller.update);
 router.delete(`${prefix}/:id`, controller.delete);
+
+router.get(`${prefix}/`, logs.all);
+router.post(`${prefix}/`, logs.create);
+router.get(`${prefix}/:id`, logs.info);
+router.put(`${prefix}/:id`, logs.update);
+router.delete(`${prefix}/:id`, logs.delete);
 
 module.exports = router;
